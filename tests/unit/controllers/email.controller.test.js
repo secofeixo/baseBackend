4c690ff7676051d7d9ec52531449c05c701c8d2c
Alberto Seco <alberto.seco@goldenspear.com>
2019-11-07 09:57:06 +0100

    fixing vulnerabilities

--- tests/unit/controllers/email.controller.test.js
+++ tests/unit/controllers/email.controller.test.js
@@ -1,17 +1,25 @@
 process.env.NODE_ENV = 'test';
-const mockRes = require('jest-mock-express').response;
 const ctrlEmail = require('../../../app/controllers/email.controller');
+
+const mockResponse = () => {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
 
 // const app = require('../app.js').getApp;
 
 describe('Testing email', () => {
+  jest.setTimeout(30000);
+
   test('Sending validate email', async () => {
     const user = {
       email: {
         addr: 'def@def.com',
       },
     };
-    const fakeRes = mockRes();
+    const fakeRes = mockResponse();
     await ctrlEmail.sendEmailValidateEmail(user, 'tokentovalidateuser', fakeRes);
     expect(fakeRes.status).toHaveBeenCalledWith(200);
     expect(fakeRes.json).toHaveBeenCalled();
@@ -25,13 +33,13 @@ describe('Testing email', () => {
         return 'Full name';
       },
     };
-    const fakeRes = mockRes();
+    const fakeRes = mockResponse();
     await ctrlEmail.sendEmailWelcomeUser(user, fakeRes);
     expect(fakeRes.status).toHaveBeenCalledWith(200);
     expect(fakeRes.json).toHaveBeenCalled();
   });
   test('sending retrieve password code', async () => {
-    const fakeRes = mockRes();
+    const fakeRes = mockResponse();
     await ctrlEmail.sendEmailRetrievePasswordCode('def@def.com', 'Code', fakeRes);
     expect(fakeRes.status).toHaveBeenCalledWith(200);
     expect(fakeRes.json).toHaveBeenCalled();
